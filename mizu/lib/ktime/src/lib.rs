a5069572df4d92193ad46e67acba60b5a1f22737
徐启航 <akucxy@163.com>
2023-03-12 23:47:42 +0800

    Improve async timer

--- mizu/lib/ktime/src/lib.rs
+++ mizu/lib/ktime/src/lib.rs
@@ -1,7 +1,13 @@
 #![cfg_attr(not(feature = "test"), no_std)]
 
+extern crate alloc;
+
 mod timer;
 
 pub use ktime_core::*;
 
-pub use self::timer::{notify_timer, Timer};
+pub use self::timer::Timer;
+
+pub fn timer_tick() {
+    timer::TIMER_QUEUE.tick();
+}
