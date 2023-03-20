8ae95843f13aafe19495f803beb55d07409fcbad
徐启航 <akucxy@163.com>
2023-03-20 23:47:38 +0800

    Tweak some code

--- mizu/lib/art/src/queue.rs
+++ mizu/lib/art/src/queue.rs
@@ -52,12 +52,15 @@ impl<T, const N: usize> Inner<T, N> {
 /// As such, `N` only exists in the following sequence:
 ///
 ///     2, 4, 8, 16, 32, 64, 128, 256
-pub struct Local<T, const N: usize> {
-    inner: Arsc<Inner<T, N>>,
-}
+pub struct Local<T, const N: usize>(Arsc<Inner<T, N>>);
+
+unsafe impl<T: Send, const N: usize> Send for Local<T, N> {}
 
 #[derive(Clone)]
 pub struct Stealer<T, const N: usize>(Arsc<Inner<T, N>>);
+
+unsafe impl<T: Send, const N: usize> Send for Stealer<T, N> {}
+unsafe impl<T: Send, const N: usize> Sync for Stealer<T, N> {}
 
 impl<T, const N: usize> Local<T, N> {
     const UNINIT: UnsafeCell<MaybeUninit<T>> = UnsafeCell::new(MaybeUninit::uninit());
@@ -74,15 +77,15 @@ impl<T, const N: usize> Local<T, N> {
             buffer: [Self::UNINIT; N],
         });
 
-        Local { inner }
+        Local(inner)
     }
 
     pub fn push(&mut self, mut value: T, backup: &SegQueue<T>) {
         let tail = loop {
-            let head = self.inner.head.load(Acquire);
+            let head = self.0.head.load(Acquire);
             let (steal, real) = unpack(head);
 
-            let tail = self.inner.tail.load(Relaxed);
+            let tail = self.0.tail.load(Relaxed);
 
             if tail.wrapping_sub(steal) < N as u32 {
                 break tail;
@@ -99,12 +102,8 @@ impl<T, const N: usize> Local<T, N> {
 
         let index = tail as usize & Self::MASK;
         // SAFETY: Only one producer.
-        unsafe {
-            self.inner.buffer[index]
-                .get()
-                .write(MaybeUninit::new(value))
-        };
-        self.inner.tail.store(tail.wrapping_add(1), Release);
+        unsafe { self.0.buffer[index].get().write(MaybeUninit::new(value)) };
+        self.0.tail.store(tail.wrapping_add(1), Release);
     }
 
     fn push_contended(
@@ -123,12 +122,9 @@ impl<T, const N: usize> Local<T, N> {
         let prev = pack(head, head);
         let next_head = head.wrapping_add(Self::BATCH_TO_BACKUP);
         let next = pack(next_head, next_head);
-        if self
-            .inner
-            .head
-            .compare_exchange(prev, next, Release, Relaxed)
-            .is_err()
-        {
+
+        let res = self.0.head.compare_exchange(prev, next, Release, Relaxed);
+        if res.is_err() {
             return Err(value);
         }
 
@@ -136,7 +132,7 @@ impl<T, const N: usize> Local<T, N> {
         let batch = (head..next_head).map(|head| {
             let index = head as usize & Self::MASK;
             // SAFETY: Successful CAS assumed ownership of these values.
-            unsafe { self.inner.buffer[index].get().read().assume_init() }
+            unsafe { self.0.buffer[index].get().read().assume_init() }
         });
         batch.for_each(|value| backup.push(value));
 
@@ -144,11 +140,11 @@ impl<T, const N: usize> Local<T, N> {
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        let mut head = self.inner.head.load(Acquire);
+        let mut head = self.0.head.load(Acquire);
         let index = loop {
             let (steal, real) = unpack(head);
 
-            let tail = self.inner.tail.load(Relaxed);
+            let tail = self.0.tail.load(Relaxed);
 
             if real == tail {
                 return None;
@@ -162,21 +158,17 @@ impl<T, const N: usize> Local<T, N> {
                 pack(steal, next_real)
             };
 
-            match self
-                .inner
-                .head
-                .compare_exchange(head, next, AcqRel, Acquire)
-            {
+            match self.0.head.compare_exchange(head, next, AcqRel, Acquire) {
                 Ok(_) => break real as usize & Self::MASK,
                 Err(h) => head = h,
             }
         };
         // SAFETY: Successful CAS assumed ownership of the value.
-        Some(unsafe { self.inner.buffer[index].get().read().assume_init() })
+        Some(unsafe { self.0.buffer[index].get().read().assume_init() })
     }
 
     pub fn stealer(&self) -> Stealer<T, N> {
-        Stealer(self.inner.clone())
+        Stealer(self.0.clone())
     }
 }
 
@@ -185,9 +177,6 @@ impl<T, const N: usize> Default for Local<T, N> {
         Self::new()
     }
 }
-
-unsafe impl<T: Send, const N: usize> Send for Stealer<T, N> {}
-unsafe impl<T: Send, const N: usize> Sync for Stealer<T, N> {}
 
 impl<T, const N: usize> Stealer<T, N> {
     const MASK: usize = Inner::<T, N>::MASK;
@@ -200,10 +189,14 @@ impl<T, const N: usize> Stealer<T, N> {
         self.0.is_empty()
     }
 
-    pub fn steal_into_and_pop(&self, dst: &mut Local<T, N>) -> Option<T> {
-        let dst_tail = dst.inner.tail.load(Relaxed);
+    pub fn steals_from(&self, queue: &Local<T, N>) -> bool {
+        Arsc::ptr_eq(&self.0, &queue.0)
+    }
 
-        let (steal, _) = unpack(dst.inner.head.load(Acquire));
+    pub fn steal_into_and_pop(&self, dst: &mut Local<T, N>) -> Option<T> {
+        let dst_tail = dst.0.tail.load(Relaxed);
+
+        let (steal, _) = unpack(dst.0.head.load(Acquire));
         if dst_tail.wrapping_sub(steal) > N as u32 / 2 {
             return None;
         }
@@ -215,26 +208,26 @@ impl<T, const N: usize> Stealer<T, N> {
         n -= 1;
         let index = dst_tail.wrapping_add(n) as usize & Self::MASK;
         // SAFETY: Successful CAS assumed ownership of the value.
-        let value = unsafe { dst.inner.buffer[index].get().read().assume_init() };
+        let value = unsafe { dst.0.buffer[index].get().read().assume_init() };
 
         if n != 0 {
-            dst.inner.tail.store(dst_tail.wrapping_add(n), Release);
+            dst.0.tail.store(dst_tail.wrapping_add(n), Release);
         }
 
         Some(value)
     }
 
     pub fn steal_into(&self, dst: &mut Local<T, N>) {
-        let dst_tail = dst.inner.tail.load(Relaxed);
+        let dst_tail = dst.0.tail.load(Relaxed);
 
-        let (steal, _) = unpack(dst.inner.head.load(Acquire));
+        let (steal, _) = unpack(dst.0.head.load(Acquire));
         if dst_tail.wrapping_sub(steal) > N as u32 / 2 {
             return;
         }
 
         let n = self.steal_n(dst, dst_tail);
         if n != 0 {
-            dst.inner.tail.store(dst_tail.wrapping_add(n), Release);
+            dst.0.tail.store(dst_tail.wrapping_add(n), Release);
         }
     }
 
@@ -276,7 +269,7 @@ impl<T, const N: usize> Stealer<T, N> {
             // SAFETY: Successful CAS assumed ownership of these values.
             unsafe {
                 let value = self.0.buffer[src_index].get().read();
-                dst.inner.buffer[dst_index].get().write(value)
+                dst.0.buffer[dst_index].get().write(value)
             }
         }
 
