5dc1d6b264d9fd0ce411e4b488fbdc9fddef877e
徐启航 <akucxy@163.com>
2023-03-28 19:12:36 +0800

    Update future timeout

--- mizu/lib/ktime/src/lib.rs
+++ mizu/lib/ktime/src/lib.rs
@@ -44,22 +44,66 @@ where
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
-        match this.fut.poll(cx) {
-            Poll::Ready(data) => Poll::Ready(data),
-            Poll::Pending => match this.timer.poll(cx) {
-                Poll::Ready(_) => Poll::Ready((this.out.take().unwrap())().into()),
-                Poll::Pending => Poll::Pending,
-            },
+        if let Poll::Ready(data) = this.fut.poll(cx) {
+            Poll::Ready(data)
+        } else if this.timer.poll(cx).is_ready() {
+            Poll::Ready((this.out.take().unwrap())().into())
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+#[must_use = "futures do nothing unless polled"]
+#[pin_project]
+pub struct OkOrTimeout<F, G> {
+    #[pin]
+    fut: F,
+    timer: Timer,
+    out: Option<G>,
+}
+
+impl<F, G, E> Future for OkOrTimeout<F, G>
+where
+    F: Future,
+    G: FnOnce() -> E,
+{
+    type Output = Result<F::Output, E>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        if let Poll::Ready(data) = this.fut.poll(cx) {
+            Poll::Ready(Ok(data))
+        } else if this.timer.poll(cx).is_ready() {
+            Poll::Ready(Err((this.out.take().unwrap())()))
+        } else {
+            Poll::Pending
         }
     }
 }
 
 pub trait TimeOutExt: Future + Sized {
-    fn on_timeout<G>(self, timer: impl Into<Timer>, out: G) -> OnTimeout<Self, G> {
+    fn on_timeout<T, G: FnOnce() -> T>(
+        self,
+        timer: impl Into<Timer>,
+        out: G,
+    ) -> OnTimeout<Self, G> {
         OnTimeout {
             fut: self,
             timer: timer.into(),
             out: Some(out),
+        }
+    }
+
+    fn ok_or_timeout<E, G: FnOnce() -> E>(
+        self,
+        timer: impl Into<Timer>,
+        err: G,
+    ) -> OkOrTimeout<Self, G> {
+        OkOrTimeout {
+            fut: self,
+            timer: timer.into(),
+            out: Some(err),
         }
     }
 }
