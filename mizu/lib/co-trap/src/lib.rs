c406fb38be39a55ec0f6a8a8014cf240955d5b23
徐启航 <akucxy@163.com>
2023-04-03 00:00:43 +0800

    Add basic syscall handling

--- mizu/lib/co-trap/src/lib.rs
+++ mizu/lib/co-trap/src/lib.rs
@@ -1,46 +1,17 @@
 #![cfg_attr(target_arch = "riscv64", no_std)]
 #![feature(const_trait_impl)]
 
+mod tf;
+
 use riscv::register::{
     scause::Scause,
     stvec::{self, Stvec, TrapMode},
 };
-use static_assertions::const_assert_eq;
+
+pub use self::tf::*;
 
 #[cfg(target_arch = "riscv64")]
 core::arch::global_asm!(include_str!("imp.S"));
-
-#[derive(Debug, Clone, Copy, Default)]
-#[repr(C)]
-pub struct Tx {
-    pub ra: usize,     // 12
-    pub sp: usize,     // 13
-    pub gp: usize,     // 14
-    pub tp: usize,     // 15
-    pub a: [usize; 8], // 16..24
-    pub t: [usize; 7], // 24..31
-}
-
-#[derive(Debug, Clone, Copy, Default)]
-#[repr(C)]
-pub struct Gpr {
-    pub s: [usize; 12], // 0..12
-    pub tx: Tx,         // 12..31
-}
-const_assert_eq!(
-    core::mem::size_of::<Gpr>(),
-    core::mem::size_of::<usize>() * 31
-);
-
-#[derive(Clone, Copy, Debug, Default)]
-#[repr(C)]
-pub struct TrapFrame {
-    pub gpr: Gpr,       // 0..31
-    pub sepc: usize,    // 31
-    pub sstatus: usize, // 32
-    pub stval: usize,   // 33
-    pub scause: usize,  // 34
-}
 
 pub fn user_entry() -> usize {
     extern "C" {
---
+++ mizu/lib/ksc/src/handler.rs
@@ -0,0 +1,175 @@
+use alloc::boxed::Box;
+use core::{
+    future::Future,
+    marker::PhantomData,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use ahash::RandomState;
+use co_trap::TrapFrame;
+use futures_util::FutureExt;
+use hashbrown::HashMap;
+
+use crate::RawReg;
+
+pub trait Handler: 'static {
+    fn handle(&self, tf: &mut TrapFrame);
+}
+
+pub trait AsyncHandler: 'static {
+    fn handle<'a>(&self, tf: &'a mut TrapFrame) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>>;
+}
+
+pub trait IntoHandler<Param> {
+    type Handler: Handler;
+
+    fn into_handler(self) -> Self::Handler;
+}
+
+pub trait IntoAsyncHandler<Param> {
+    type Handler: AsyncHandler;
+
+    fn into_handler(self) -> Self::Handler;
+}
+
+pub struct FunctionHandler<F, Args> {
+    func: F,
+    _param: PhantomData<Args>,
+}
+
+macro_rules! impl_fn {
+    ($($arg:ident),*) => {
+        impl<Z, $($arg: RawReg + 'static,)* R: RawReg> Handler for FunctionHandler<Z, ($($arg,)*)>
+        where
+            Z: Fn($($arg),*) -> R + 'static,
+        {
+            fn handle(&self, tf: &mut TrapFrame) {
+                $(
+                    #[allow(non_snake_case)]
+                    let $arg = tf.syscall_arg::<${index()}>();
+                )*
+                let r = (self.func)($(<$arg as RawReg>::from_raw($arg)),*);
+                tf.set_syscall_ret(R::into_raw(r))
+            }
+        }
+
+        impl<Z, $($arg: RawReg + 'static,)* R: RawReg> IntoHandler<($($arg,)*)> for Z
+        where
+            Z: Fn($($arg),*) -> R + 'static,
+        {
+            type Handler = FunctionHandler<Z, ($($arg,)*)>;
+
+            fn into_handler(self) -> Self::Handler {
+                FunctionHandler {
+                    func: self,
+                    _param: PhantomData,
+                }
+            }
+        }
+
+        impl<Z, $($arg: RawReg + 'static,)* R> AsyncHandler for FunctionHandler<Z, ($($arg,)*)>
+        where
+            R: Future + Send + 'static,
+            R::Output: RawReg,
+            Z: Fn($($arg),*) -> R + 'static,
+        {
+            fn handle<'a>(&self, tf: &'a mut TrapFrame) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>> {
+                $(
+                    #[allow(non_snake_case)]
+                    let $arg = tf.syscall_arg::<${index()}>();
+                )*
+                let r = (self.func)($(<$arg as RawReg>::from_raw($arg)),*);
+                Box::pin(r.map(move |ret| tf.set_syscall_ret(R::Output::into_raw(ret))))
+            }
+        }
+
+        impl<Z, $($arg: RawReg + 'static,)* R> IntoAsyncHandler<($($arg,)*)> for Z
+        where
+            R: Future + Send + 'static,
+            R::Output: RawReg,
+            Z: Fn($($arg),*) -> R + 'static,
+        {
+            type Handler = FunctionHandler<Z, ($($arg,)*)>;
+
+            fn into_handler(self) -> Self::Handler {
+                FunctionHandler {
+                    func: self,
+                    _param: PhantomData,
+                }
+            }
+        }
+    };
+}
+
+impl_fn!(A);
+impl_fn!(A, B);
+impl_fn!(A, B, C);
+impl_fn!(A, B, C, D);
+impl_fn!(A, B, C, D, E);
+impl_fn!(A, B, C, D, E, F);
+impl_fn!(A, B, C, D, E, F, G);
+
+pub struct Handlers {
+    map: HashMap<u8, Box<dyn Handler>, RandomState>,
+}
+
+impl Handlers {
+    pub fn new(seed: usize) -> Self {
+        Handlers {
+            map: HashMap::with_hasher(RandomState::with_seed(seed)),
+        }
+    }
+
+    pub fn with<P>(mut self, hn: u8, handler: impl IntoHandler<P>) -> Self {
+        self.map.insert(hn, Box::new(handler.into_handler()));
+        self
+    }
+
+    pub fn handle(&self, tf: &mut TrapFrame) {
+        let hn = u8::try_from(tf.syscall_arg::<7>());
+        let handler = hn.ok().and_then(|hn| self.map.get(&hn));
+        if let Some(handler) = handler {
+            handler.handle(tf);
+        }
+    }
+}
+
+pub struct AsyncHandlers {
+    map: HashMap<u8, Box<dyn AsyncHandler>, RandomState>,
+}
+
+impl AsyncHandlers {
+    pub fn new(seed: usize) -> Self {
+        AsyncHandlers {
+            map: HashMap::with_hasher(RandomState::with_seed(seed)),
+        }
+    }
+
+    pub fn with<P>(mut self, hn: u8, handler: impl IntoAsyncHandler<P>) -> Self {
+        self.map.insert(hn, Box::new(handler.into_handler()));
+        self
+    }
+
+    pub fn handle<'a>(&self, tf: &'a mut TrapFrame) -> Handle<'a> {
+        let hn = u8::try_from(tf.syscall_arg::<7>());
+        let handler = hn.ok().and_then(|hn| self.map.get(&hn));
+        Handle(handler.map(|handler| handler.handle(tf)))
+    }
+}
+
+#[must_use = "futures do nothing unless polled"]
+pub struct Handle<'a>(Option<Pin<Box<dyn Future<Output = ()> + Send + 'a>>>);
+
+impl Unpin for Handle<'_> {}
+
+impl Future for Handle<'_> {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.0.as_mut() {
+            Some(fut) => fut.poll_unpin(cx),
+            None => Poll::Ready(()),
+        }
+    }
+}
