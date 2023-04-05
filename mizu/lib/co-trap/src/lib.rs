8f1f784f89a6b0329451b411d8842212f5366772
徐启航 <akucxy@163.com>
2023-04-05 10:25:56 +0800

    Add `FastResult`

--- mizu/lib/co-trap/src/lib.rs
+++ mizu/lib/co-trap/src/lib.rs
@@ -4,6 +4,8 @@
 
 mod tf;
 
+use enum_primitive_derive::Primitive;
+use num_traits::FromPrimitive;
 use riscv::register::{
     scause::Scause,
     stvec::{self, Stvec, TrapMode},
@@ -46,33 +48,52 @@ impl Drop for StvecTemp {
     }
 }
 
-pub fn yield_to_user(frame: &mut TrapFrame) -> (Scause, usize) {
+pub fn yield_to_user(frame: &mut TrapFrame) -> (Scause, FastResult) {
     extern "C" {
         fn _return_to_user(frame: *mut TrapFrame) -> usize;
     }
 
     ksync_core::critical(|| unsafe {
         let _stvec = StvecTemp::new(user_entry(), TrapMode::Direct);
-        let status = _return_to_user(frame);
-        (core::mem::transmute(frame.scause), status)
+        let res = _return_to_user(frame);
+        (
+            core::mem::transmute(frame.scause),
+            FastResult::from_usize(res).unwrap(),
+        )
     })
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Primitive)]
+#[repr(usize)]
+pub enum FastResult {
+    /// Finished fast-path execution without error, and directly yield to user.
+    Yield = 0,
+    /// The fast-path execution cannot be done, should do the ordinary path in
+    /// the async context.
+    Continue = 1,
+    /// Finished fast-path execution without error, but the current task has
+    /// some pending events (signals).
+    Pending = 2,
+    /// Should directly exit the current task.
+    Break = 3,
 }
 
 #[doc(hidden)]
 #[repr(C)]
 pub struct FastRet {
     pub cx: &'static mut TrapFrame,
-    pub status: usize,
+    pub res: FastResult,
 }
 
 /// Set the fast path function (conventional trap handler).
 ///
 /// ```
-/// fn some_fast_func(_cx: &mut co_trap::TrapFrame) -> usize {
-///     1 // 0 means returning directly to the user thread, while others will
-///       // be passed to the normal coroutine context.
+/// use co_trap::{TrapFrame, FastResult, fast_func};
+///
+/// fn some_fast_func(_cx: &mut TrapFrame) -> FastResult {
+///     FastResult::Continue
 /// }
-/// co_trap::fast_func!(some_fast_func);
+/// fast_func!(some_fast_func);
 /// ```
 ///
 /// If a fast path is not desired, just use `co_trap::fast_func!()` instead.
@@ -83,14 +104,17 @@ macro_rules! fast_func {
     ($func:ident) => {
         #[no_mangle]
         extern "C" fn _fast_func(cx: &'static mut $crate::TrapFrame) -> $crate::FastRet {
-            let status = ($func)(cx);
-            $crate::FastRet { cx, status }
+            let res = ($func)(cx);
+            $crate::FastRet { cx, res }
         }
     };
     () => {
         #[no_mangle]
         extern "C" fn _fast_func(cx: &'static mut $crate::TrapFrame) -> $crate::FastRet {
-            $crate::FastRet { cx, status: 1 }
+            $crate::FastRet {
+                cx,
+                res: $crate::FastResult::Continue,
+            }
         }
     };
 }
