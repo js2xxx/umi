20ac95f95ce1b6de24cc227df971b291525a479d
徐启航 <akucxy@163.com>
2023-04-06 19:21:38 +0800

    Add custom `RandomState`

--- mizu/lib/rand-riscv/src/lib.rs
+++ mizu/lib/rand-riscv/src/lib.rs
@@ -1,4 +1,12 @@
 #![no_std]
+#![feature(build_hasher_simple_hash_one)]
+#![feature(const_trait_impl)]
+
+use core::{
+    fmt,
+    hash::{BuildHasher, Hash},
+    marker::Destruct,
+};
 
 pub use rand_chacha::rand_core;
 use rand_chacha::rand_core::SeedableRng;
@@ -48,4 +56,50 @@ pub fn rng() -> Rng {
         self::seed(&mut seed);
         seed
     })
+}
+
+/// A wrapper around `ahash::RandomState`, using built-in seeds.
+pub struct RandomState(ahash::RandomState);
+
+impl fmt::Debug for RandomState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RandomState").finish_non_exhaustive()
+    }
+}
+
+impl RandomState {
+    pub fn new() -> Self {
+        #[cfg(not(feature = "test"))]
+        return RandomState(ahash::RandomState::with_seeds(
+            seed64(),
+            seed64(),
+            seed64(),
+            seed64(),
+        ));
+        #[cfg(feature = "test")]
+        RandomState(ahash::RandomState::with_seed(rand::random()))
+    }
+
+    #[inline]
+    pub fn hash_one<T: Hash>(&self, x: T) -> u64 {
+        self.0.hash_one(x)
+    }
+}
+
+impl Default for RandomState {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl BuildHasher for RandomState {
+    type Hasher = ahash::AHasher;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        self.0.build_hasher()
+    }
+
+    fn hash_one<T: ~const Hash + ~const Destruct>(&self, x: T) -> u64 {
+        self.0.hash_one(x)
+    }
 }
