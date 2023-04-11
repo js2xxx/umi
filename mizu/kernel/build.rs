0b668ef44d8e2ff0759612375e09ed03ecd13004
徐启航 <akucxy@163.com>
2023-04-11 23:18:17 +0800

    Update link options

--- mizu/kernel/build.rs
+++ mizu/kernel/build.rs
@@ -6,11 +6,14 @@ fn main() {
         let out_dir = env::var("OUT_DIR").unwrap();
         let ldscript = fs::read_to_string(PathBuf::from(manifest_dir).join("link.ld")).unwrap();
 
-        let new = ldscript.replace("%KERNEL_START%", &config::KERNEL_START.to_string());
-        let new = new.replace(
-            "%KERNEL_START_PHYS%",
-            &config::KERNEL_START_PHYS.to_string(),
-        );
+        // Reduce the prefix region sized `KERNEL_OFFSET` to avoid overlapping with the
+        // firmware.
+
+        let ram_size = config::RAM_SIZE - config::KERNEL_OFFSET;
+
+        let new = ldscript.replace("%RAM_START%", &config::KERNEL_START_PHYS.to_string());
+        let new = new.replace("%VIRT_START%", &config::KERNEL_START.to_string());
+        let new = new.replace("%RAM_SIZE%", &ram_size.to_string());
         let new = new.replace("%MAX_HARTS%", &config::MAX_HARTS.to_string());
 
         let dest = PathBuf::from(out_dir).join("link.ld");
