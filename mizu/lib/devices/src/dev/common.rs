6d36d7ada0a91009ce53f334b24ae5615c74cd27
徐启航 <akucxy@163.com>
2023-04-30 09:28:28 +0800

    Fix update

--- mizu/lib/devices/src/dev/common.rs
+++ mizu/lib/devices/src/dev/common.rs
@@ -28,7 +28,7 @@ pub use virtio_drivers::{BufferDirection, Hal as VirtioHal};
 
 pub struct HalImpl;
 
-impl VirtioHal for HalImpl {
+unsafe impl VirtioHal for HalImpl {
     fn dma_alloc(pages: usize, _: BufferDirection) -> (virtio_drivers::PhysAddr, NonNull<u8>) {
         match NonZeroUsize::new(pages).and_then(|count| kmem::frames().allocate(count)) {
             Some(addr) => (*addr.to_paddr(ID_OFFSET), unsafe {
@@ -38,7 +38,7 @@ impl VirtioHal for HalImpl {
         }
     }
 
-    fn dma_dealloc(_: virtio_drivers::PhysAddr, ptr: NonNull<u8>, pages: usize) -> i32 {
+    unsafe fn dma_dealloc(_: virtio_drivers::PhysAddr, ptr: NonNull<u8>, pages: usize) -> i32 {
         if let Some(count) = NonZeroUsize::new(pages) {
             let addr = LAddr::from(ptr);
             unsafe { kmem::frames().deallocate(addr, count) }
@@ -46,14 +46,14 @@ impl VirtioHal for HalImpl {
         0
     }
 
-    fn mmio_phys_to_virt(paddr: virtio_drivers::PhysAddr, _: usize) -> NonNull<u8> {
+    unsafe fn mmio_phys_to_virt(paddr: virtio_drivers::PhysAddr, _: usize) -> NonNull<u8> {
         let laddr = PAddr::new(paddr).to_laddr(ID_OFFSET);
         laddr.as_non_null().expect("invalid address")
     }
 
-    fn share(buffer: NonNull<[u8]>, _: BufferDirection) -> virtio_drivers::PhysAddr {
+    unsafe fn share(buffer: NonNull<[u8]>, _: BufferDirection) -> virtio_drivers::PhysAddr {
         *LAddr::from(buffer).to_paddr(ID_OFFSET)
     }
 
-    fn unshare(_: virtio_drivers::PhysAddr, _: NonNull<[u8]>, _: BufferDirection) {}
+    unsafe fn unshare(_: virtio_drivers::PhysAddr, _: NonNull<[u8]>, _: BufferDirection) {}
 }
